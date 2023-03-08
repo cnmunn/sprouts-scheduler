c00450fdbc4836e081c6d42dfac512436d47ba81
lamolivi <olivia.lam@alumni.ubc.ca>
2023-03-07 21:03:29 -0800

    revamped everything

---
+++ my-app/src/demo.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import {
+  Scheduler,
+  WeekView,
+  Appointments,
+  AppointmentTooltip,
+  AppointmentForm,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+import appointments from './demo-data/today-appointments';
+
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: appointments,
+    };
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <Paper>
+        <Scheduler
+          data={data}
+          height={1000}
+        >
+          <WeekView
+            startDayHour={9}
+            endDayHour={19}
+          />
+
+          <Appointments />
+          <AppointmentTooltip
+            showCloseButton
+            showOpenButton
+          />
+          <AppointmentForm
+            readOnly
+          />
+        </Scheduler>
+      </Paper>
+    );
+  }
+}
