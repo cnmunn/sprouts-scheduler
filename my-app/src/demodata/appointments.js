87a32aa5b7610b73f46965541f2801ebf6c90b14
lamolivi <olivia.lam@alumni.ubc.ca>
2023-03-21 16:08:21 -0700

    accurate data

--- my-app/src/demodata/appointments.js
+++ my-app/src/demodata/appointments.js
@@ -1,4 +1,12 @@
 export const appointments = [
+  {
+    title: 'Shift',
+    location: 'Cafe',
+    startDate: new Date(2022, 5, 20, 9, 0),
+    endDate: new Date(2022, 5, 20, 11, 0),
+    rRule: 'FREQ=WEEKLY',
+    id: 0
+  },
     {
       title: 'Shift',
       location: 'Cafe',
@@ -10,26 +18,107 @@ export const appointments = [
     {
       title: 'Shift',
       location: 'Cafe',
-      startDate: new Date(2022, 5, 20, 13, 30),
-      endDate: new Date(2022, 5, 20, 15, 30),
+      startDate: new Date(2022, 5, 20, 13, 0),
+      endDate: new Date(2022, 5, 20, 15, 0),
       rRule: 'FREQ=WEEKLY',
       id: 2
     },
     {
       title: 'Shift',
       location: 'Cafe',
-      startDate: new Date(2022, 5, 21, 9, 30),
-      endDate: new Date(2022, 5, 21, 11, 30),
+      startDate: new Date(2022, 5, 20, 15, 0),
+      endDate: new Date(2022, 5, 20, 17, 0),
       rRule: 'FREQ=WEEKLY',
       id: 3
     },
     {
       title: 'Shift',
       location: 'Cafe',
-      startDate: new Date(2022, 5, 22, 9, 30),
-      endDate: new Date(2022, 5, 22, 11, 30),
+      startDate: new Date(2022, 5, 21, 9, 0),
+      endDate: new Date(2022, 5, 21, 11, 0),
       rRule: 'FREQ=WEEKLY',
       id: 4
+    },
+      {
+        title: 'Shift',
+        location: 'Cafe',
+        startDate: new Date(2022, 5, 21, 11, 0),
+        endDate: new Date(2022, 5, 21, 13, 0),
+        rRule: 'FREQ=WEEKLY',
+        id: 5
+      },
+      {
+        title: 'Shift',
+        location: 'Cafe',
+        startDate: new Date(2022, 5, 21, 13, 0),
+        endDate: new Date(2022, 5, 21, 15, 0),
+        rRule: 'FREQ=WEEKLY',
+        id: 6
+      },
+      {
+        title: 'Shift',
+        location: 'Cafe',
+        startDate: new Date(2022, 5, 21, 15, 0),
+        endDate: new Date(2022, 5, 21, 17, 0),
+        rRule: 'FREQ=WEEKLY',
+        id: 7
+      },
+    {
+      title: 'Shift',
+      location: 'Cafe',
+      startDate: new Date(2022, 5, 22, 9, 0),
+      endDate: new Date(2022, 5, 22, 11, 0),
+      rRule: 'FREQ=WEEKLY',
+      id: 8
+    },
+    {
+      title: 'Shift',
+      location: 'Cafe',
+      startDate: new Date(2022, 5, 22, 11, 0),
+      endDate: new Date(2022, 5, 22, 13, 0),
+      rRule: 'FREQ=WEEKLY',
+      id: 9
+    },
+    {
+      title: 'Shift',
+      location: 'Cafe',
+      startDate: new Date(2022, 5, 22, 13, 0),
+      endDate: new Date(2022, 5, 22, 15, 0),
+      rRule: 'FREQ=WEEKLY',
+      id: 10
+    },
+    {
+      title: 'Shift',
+      location: 'Cafe',
+      startDate: new Date(2022, 5, 22, 15, 0),
+      endDate: new Date(2022, 5, 22, 17, 0),
+      rRule: 'FREQ=WEEKLY',
+      id: 11
+    },
+    {
+      title: 'Shift',
+      location: 'Cafe',
+      startDate: new Date(2022, 5, 23, 9, 0),
+      endDate: new Date(2022, 5, 23, 11, 0),
+      rRule: 'FREQ=WEEKLY',
+      id: 12
+    },
+    {
+      title: 'Shift',
+      location: 'Cafe',
+      startDate: new Date(2022, 5, 23, 11, 0),
+      endDate: new Date(2022, 5, 23, 13, 0),
+      rRule: 'FREQ=WEEKLY',
+      id: 13
+    },
+    {
+      title: 'Shift',
+      location: 'Cafe',
+      startDate: new Date(2022, 5, 23, 13, 0),
+      endDate: new Date(2022, 5, 23, 15, 0),
+      rRule: 'FREQ=WEEKLY',
+      id: 14
+      
     },
     {
       title: 'Shift',
@@ -37,32 +126,15 @@ export const appointments = [
       startDate: new Date(2022, 5, 23, 15, 0),
       endDate: new Date(2022, 5, 23, 17, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 5
-    },
-    {
-      title: 'Shift',
-      location: 'Cafe',
-      startDate: new Date(2022, 5, 24, 9, 30),
-      endDate: new Date(2022, 5, 24, 11, 30),
-      rRule: 'FREQ=WEEKLY',
-      id: 6
+      id: 15
     },
     {
       title: 'Shift',
       location: 'Fridge',
-      startDate: new Date(2022, 5, 20, 9, 30),
-      endDate: new Date(2022, 5, 20, 11, 30),
+      startDate: new Date(2022, 5, 19, 18, 0),
+      endDate: new Date(2022, 5, 19, 20, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 7
-      
-    },
-    {
-      title: 'Shift',
-      location: 'Fridge',
-      startDate: new Date(2022, 5, 20, 13, 30),
-      endDate: new Date(2022, 5, 20, 15, 30),
-      rRule: 'FREQ=WEEKLY',
-      id: 8
+      id: 16
     },
     {
       title: 'Shift',
@@ -70,7 +142,7 @@ export const appointments = [
       startDate: new Date(2022, 5, 21, 18, 0),
       endDate: new Date(2022, 5, 21, 20, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 9
+      id: 17
     },
     {
       title: 'Shift',
@@ -78,39 +150,23 @@ export const appointments = [
       startDate: new Date(2022, 5, 22, 13, 30),
       endDate: new Date(2022, 5, 22, 15, 30),
       rRule: 'FREQ=WEEKLY',
-      id: 10
+      id: 18
     },
     {
       title: 'Shift',
       location: 'Fridge',
-      startDate: new Date(2022, 5, 23, 9, 30),
-      endDate: new Date(2022, 5, 23, 11, 30),
+      startDate: new Date(2022, 5, 23, 18, 0),
+      endDate: new Date(2022, 5, 23, 20, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 11
+      id: 19
     },
     {
       title: 'Shift',
       location: 'Fridge',
-      startDate: new Date(2022, 5, 24, 9, 30),
-      endDate: new Date(2022, 5, 24, 11, 30),
+      startDate: new Date(2022, 5, 24, 18, 0),
+      endDate: new Date(2022, 5, 24, 20, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 12
-    },
-    {
-      title: 'Shift',
-      location: 'Distro',
-      startDate: new Date(2022, 5, 19, 10, 0),
-      endDate: new Date(2022, 5, 19, 12, 0),
-      rRule: 'FREQ=WEEKLY',
-      id: 13
-    },
-    {
-      title: 'Shift',
-      location: 'Distro',
-      startDate: new Date(2022, 5, 19, 11, 0),
-      endDate: new Date(2022, 5, 19, 13, 0),
-      rRule: 'FREQ=WEEKLY',
-      id: 14
+      id: 20
     },
     {
       title: 'Shift',
@@ -118,7 +174,15 @@ export const appointments = [
       startDate: new Date(2022, 5, 25, 10, 0),
       endDate: new Date(2022, 5, 25, 12, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 15
+      id: 21
+    },
+    {
+      title: 'Shift',
+      location: 'Distro',
+      startDate: new Date(2022, 5, 25, 12, 0),
+      endDate: new Date(2022, 5, 25, 14, 0),
+      rRule: 'FREQ=WEEKLY',
+      id: 22
     },
     {
       title: 'Shift',
@@ -126,15 +190,7 @@ export const appointments = [
       startDate: new Date(2022, 5, 25, 14, 0),
       endDate: new Date(2022, 5, 25, 16, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 16
-    },
-    {
-      title: 'Shift',
-      location: 'Distro',
-      startDate: new Date(2022, 5, 22, 14, 0),
-      endDate: new Date(2022, 5, 22, 16, 0),
-      rRule: 'FREQ=WEEKLY',
-      id: 17
+      id: 23
     },
     {
       title: 'Shift',
@@ -142,15 +198,15 @@ export const appointments = [
       startDate: new Date(2022, 5, 24, 9, 0),
       endDate: new Date(2022, 5, 24, 11, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 18
+      id: 24
     },
     {
       title: 'Shift',
       location: 'Eats',
-      startDate: new Date(2022, 5, 24, 12, 0),
+      startDate: new Date(2022, 5, 24, 11, 0),
       endDate: new Date(2022, 5, 24, 14, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 19
+      id: 25
     },
     {
       title: 'Shift',
@@ -158,11 +214,8 @@ export const appointments = [
       startDate: new Date(2022, 5, 24, 14, 0),
       endDate: new Date(2022, 5, 24, 16, 0),
       rRule: 'FREQ=WEEKLY',
-      id: 20
+      id: 26
     },
-
-
-
 
   ];
   
